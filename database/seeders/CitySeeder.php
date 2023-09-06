8fe89e768264c34ee63011f411f1ec68eca73adf
Rostick0 <87297459+Rostick0@users.noreply.github.com>
2023-09-06 22:31:28 +0300

    +add mcrR for post, city, image & seeder for city

---
+++ database/seeders/CitySeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\City;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CitySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $cities = [
+            [
+                'name' => 'Россия'
+            ],
+            [
+                'name' => 'Турция'
+            ],
+            [
+                'name' => 'Испания'
+            ]
+        ];
+
+        foreach ($cities as $city) {
+            City::create($city);
+        }
+    }
+}
