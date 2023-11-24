271983ecce68592d60e2d145ca336d5b87c10449
Rostick0 <87297459+Rostick0@users.noreply.github.com>
2023-11-24 17:10:03 +0300

    +add last message for chat

--- app/Models/Chat.php
+++ app/Models/Chat.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Chat extends Model
@@ -25,5 +26,9 @@ class Chat extends Model
     public function chat_users(): HasMany
     {
         return $this->hasMany(ChatUser::class, 'chat_id ', 'id');
+    }
+
+    public function last_message(): HasOne {
+        return $this->hasOne(Message::class)->latestOfMany();
     }
 }
