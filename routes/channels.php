d8b395f653db37e0a973a53e3a1afb5e5e37c0e0
Rostick0 <87297459+Rostick0@users.noreply.github.com>
2023-11-24 18:15:48 +0300

    +add docker compose socketi, redis:

--- routes/channels.php
+++ routes/channels.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Broadcast;
 |
 */
 
+Broadcast::routes(['middleware' => ['auth']]);
+
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
+});
+
+Broadcast::channel('message.{user_id}', function ($user, $user_id) {
+    return (int) $user->id === (int) $user_id;
 });
