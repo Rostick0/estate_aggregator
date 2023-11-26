dd9e37f2696d585753670d1b58cd3d17e4884596
Rostick0 <87297459+Rostick0@users.noreply.github.com>
2023-11-26 17:03:39 +0300

    +add docs for message

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -15,6 +15,17 @@ use Illuminate\Http\Request;
 
 class MessageController extends Controller
 {
+    private static function getWhere()
+    {
+        $where = [];
+
+        if (auth()?->user()?->role !== 'admin') {
+            $where[] = ['user_id', '=', auth()?->id(), 'chat.chat_users'];
+        }
+
+        return $where;
+    }
+
     private static function extendsMutation($data, $request)
     {
         $data->images()->delete();
@@ -36,14 +47,128 @@ class MessageController extends Controller
         }
     }
 
+    /**
+     * Index
+     * @OA\get (
+     *     path="/api/message",
+     *     tags={"Message"},
+     *     security={{"bearer_token": {}}},
+     *     @OA\Parameter(
+     *          name="filter",
+     *          in="query",
+     *          @OA\Schema(
+     *              type="object",
+     *              example={
+     *                 "filter[id]":null,
+     *                 "filter[content]":null,
+     *                 "filter[is_read]":null,
+     *                 "filter[chat_id]":null,
+     *                 "filter[user_id]":null,
+     *                 "filter[created_at]":null,
+     *                 "filter[updated_at]":null,
+     *               }
+     *          )
+     *      ),
+     *     @OA\Parameter(
+     *          name="sort",
+     *          description="Сортировка по параметру",
+     *          in="query",
+     *          example="id",
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *      ),
+     *      @OA\Parameter(
+     *          name="page",
+     *          description="Page",
+     *          in="query",
+     *          example="2",
+     *          @OA\Schema(
+     *              type="number"
+     *          )
+     *      ),
+     *      @OA\Parameter(
+     *          name="limit",
+     *          description="Limit data",
+     *          in="query",
+     *          example="20",
+     *          @OA\Schema(
+     *              type="number",
+     *          )
+     *      ),
+     *      @OA\Parameter(
+     *          name="extends",
+     *          description="Extends data",
+     *          in="query",
+     *          example="chat,user,images,files",
+     *          @OA\Schema(
+     *              type="string",
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Success",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="data", type="object",
+     *                  ref="#/components/schemas/MessageSchema"
+     *              ),
+     *          )
+     *      ),
+     * )
+     */
     public function index(IndexMessageRequest $request)
     {
         return new JsonResponse(
-            Filter::all($request, new Message)
+            Filter::all($request, new Message, [], $this::getWhere())
         );
     }
 
-
+    /**
+     * Store
+     * @OA\Post (
+     *     path="/api/message",
+     *     tags={"Message"},
+     *     security={{"bearer_token": {}}},
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                      @OA\Property(
+     *                          property="content",
+     *                          type="string",
+     *                          example="Я хочу написать",
+     *                      ),
+     *                      @OA\Property(
+     *                          property="images",
+     *                          type="string",
+     *                          example="1,2"
+     *                      ),
+     *                      @OA\Property(
+     *                          property="files",
+     *                          type="number",
+     *                          example=""
+     *                      ),
+     *              )
+     *         )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Success",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="data", type="object",
+     *                  ref="#/components/schemas/MessageSchema"
+     *              ),
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Validation error",
+     *          @OA\JsonContent(
+     *                  @OA\Property(property="message", type="string", example="The content field is required when none of images, files are present"),
+     *          )
+     *      )
+     * )
+     */
     public function store(StoreMessageRequest $request)
     {
         $data = Message::create([
@@ -58,13 +183,102 @@ class MessageController extends Controller
         ]);
     }
 
+    /**
+     * Show
+     * @OA\get (
+     *     path="/api/message/{id}",
+     *     tags={"Message"},
+     *     security={{"bearer_token": {}}},
+     *     @OA\Parameter(
+     *          name="id",
+     *          example="1",
+     *          in="path",
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *      ),
+     *      @OA\Parameter(
+     *          name="extends",
+     *          description="Extends data",
+     *          in="query",
+     *          example="chat,user,images,files",
+     *          @OA\Schema(
+     *              type="string",
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Success",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="data", type="object",
+     *                  ref="#/components/schemas/MessageSchema"
+     *              ),
+     *          )
+     *      ),
+     * )
+     */
     public function show(Request $request, int $id)
     {
         return new JsonResponse(
-            Filter::one($request, new Message, $id)
+            Filter::one($request, new Message, $id, $this::getWhere())
         );
     }
 
+    /**
+     * Update
+     * @OA\Put (
+     *     path="/api/message/{id}",
+     *     tags={"Message"},
+     *     security={{"bearer_token": {}}},
+     *     @OA\Parameter(
+     *          name="id",
+     *          description="User id",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                      @OA\Property(
+     *                          property="content",
+     *                          type="string",
+     *                          example="Я хочу написать",
+     *                      ),
+     *                      @OA\Property(
+     *                          property="images",
+     *                          type="string",
+     *                          example="1,2"
+     *                      ),
+     *                      @OA\Property(
+     *                          property="files",
+     *                          type="number",
+     *                          example=""
+     *                      ),
+     *              )
+     *         )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Success",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="data", type="object",
+     *                  ref="#/components/schemas/MessageSchema"
+     *              ),
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Validation error",
+     *          @OA\JsonContent(
+     *                  @OA\Property(property="message", type="string", example="The content field is required when none of images, files are present"),
+     *          )
+     *      )
+     * )
+     */
     public function update(UpdateMessageRequest $request, int $id)
     {
         $data = Message::findOrFail($id);
@@ -82,7 +296,37 @@ class MessageController extends Controller
         ]);
     }
 
-
+    /**
+     * Delete
+     * @OA\Delete (
+     *     path="/api/message/{id}",
+     *     tags={"Message"},
+     *     security={{"bearer_token": {}}},
+     *     @OA\Parameter(
+     *          name="id",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *     @OA\Response(
+     *          response=200,
+     *          description="Success",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="message", type="string", example="Deleted"),
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Access error",
+     *          @OA\JsonContent(
+     *                  @OA\Property(property="message", type="string", example="No access"),
+     *                 ),
+     *          )
+     *      )
+     * )
+     */
     public function destroy(int $id)
     {
         $message = Message::findOrFail($id);
--- app/Models/Message.php
+++ app/Models/Message.php
@@ -7,6 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 
+/**
+ * @OA\Schema(
+ *    schema="MessageSchema",
+ *       @OA\Property(property="id", type="number", example=1),
+ *       @OA\Property(property="content", type="string", example="Гена лучший"),
+ *       @OA\Property(property="is_read", type="boolean", example="false"),
+ *       @OA\Property(property="chat_id", type="number", example="1"),
+ *       @OA\Property(property="user_id", type="number", example="5"),
+ *       @OA\Property(property="created_at", type="datetime", example="2022-06-28 06:06:17"),
+ *       @OA\Property(property="updated_at", type="datetime", example="2022-06-28 06:06:17"),
+ *    )
+ * )
+ */
 class Message extends Model
 {
     use HasFactory;
