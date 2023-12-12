74521e620b08906b48ae4164b562ed378c86904f
Rostick0 <87297459+Rostick0@users.noreply.github.com>
2023-12-12 14:58:04 +0300

    !fix migration application user

--- database/migrations/2023_12_11_214052_create_application_users_table.php
+++ database/migrations/2023_12_11_214052_create_application_users_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('application_users', function (Blueprint $table) {
             $table->id();
             $table->enum('role', ['client', 'realtor', 'agency', 'builder']);
+            $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
