10908abf5c2757f0c6141508aa776a19bae4e439
Rostick0 <87297459+Rostick0@users.noreply.github.com>
2024-01-15 12:14:21 +0300

    !Fix sort

--- app/Utils/OrderByUtil.php
+++ app/Utils/OrderByUtil.php
@@ -2,6 +2,7 @@
 
 namespace App\Utils;
 
+use Exception;
 use Illuminate\Database\Eloquent\Builder;
 
 class OrderByUtil
@@ -32,26 +33,48 @@ class OrderByUtil
         $table = $builder->getModel()->getTable();
         $builder->select($table . '.*');
 
-        $sort_name = $name;
+        $sort_name = '';
 
         $name_array = explode('.', $name);
+        $my_relat = $builder;
+        // dd($relat = $builder->getRelation('user')->getRelation('company')->getOwnerKeyName());
 
-        if (isset($name_array[1])) {
-            $relat = $builder->getRelation(OrderByUtil::removeMinus($name_array[0]));
-            $relat_table = $relat->getModel()->getTable();
-            
-            $builder->join(
-                $relat_table,
-                $table . '.' . $relat->getForeignKeyName(),
-                '=',
-                $relat_table . '.' . $relat->getOwnerKeyName()
-            );
+        if (count($name_array) > 1) {
+            foreach ((array_slice($name_array, 1)) as $key => $value) {
+                $relat = $my_relat->getRelation(OrderByUtil::removeMinus($name_array[$key]));
+                $relat_table = $relat->getModel()->getTable();
 
-            $sort_name = $relat_table . '.' . $name_array[1];
+                $relat_parent = null;
+                $relat_child = null;
+
+                try {
+                    $relat_parent = $relat->getOwnerKeyName();
+                    $relat_child = $relat->getForeignKeyName();
+                } catch (Exception $e){
+                    $relat_child = $relat->getLocalKeyName();
+                    $relat_parent = $relat->getLocalKeyName();
+                }
+                
+                $builder->join(
+                    $relat_table,
+                    $my_relat->getModel()->getTable() . '.' . $relat_child,
+                    '=',
+                    $relat_table . '.' . $relat_parent
+                );
+
+                $sort_name = $relat_table . '.';
+                $my_relat = $relat;
+            }
         }
 
+        $sort_name .= end($name_array);
+
+        // dd($sort_name);
+
         return $builder->orderBy(
-            OrderByUtil::removeMinus($sort_name) ?? 'id',
+            OrderByUtil::removeMinus(
+                $sort_name
+            ) ?? 'id',
             OrderByUtil::type($name)
         );
     }
