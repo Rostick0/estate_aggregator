fdcf2766b2a0ff2dd2f84581dbadf782038bc603
Rostick0 <87297459+Rostick0@users.noreply.github.com>
2024-01-20 13:59:56 +0300

    !fix

--- app/Policies/RecruitmentFlatPolicy.php
+++ app/Policies/RecruitmentFlatPolicy.php
@@ -3,6 +3,7 @@
 namespace App\Policies;
 
 use App\Models\Recruitment;
+use App\Models\RecruitmentFlat;
 use App\Models\User;
 
 class RecruitmentFlatPolicy
@@ -10,5 +11,9 @@ class RecruitmentFlatPolicy
     public static function create(int $recruitment_id)
     {
         return auth()?->id() ==  Recruitment::find($recruitment_id)->user_id;
+    }
+
+    public function delete(User $user, RecruitmentFlat $recruitment_flat) {
+        return $user->role == 'admin' || $user?->id == $recruitment_flat->recruitment->user_id;
     }
 }
